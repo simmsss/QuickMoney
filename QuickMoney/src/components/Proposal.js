f7334bde4bba4e3df9def64d798429554426f737
Utkarsh Sharma <skhiearth@gmail.com>
2020-12-13 12:52:35 +0530

    Update Proposal.js

--- QuickMoney/src/components/Proposal.js
+++ QuickMoney/src/components/Proposal.js
@@ -164,7 +164,166 @@ class Proposal extends Component {
   render() {
     return (
       <div styles={{ backgroundImage:`url(${bg})`}}>
- 
+        { this.state.loading
+          ? 
+          <div className="center mt-19">
+          <div className={styles.verifyTitle}>QuickMoney</div>
+              <FingerprintSpinner
+                style={{width: "100%"}}
+                color='#251F82'
+                size='200'
+	            />
+                <div style={{paddingTop: 10, textAlign: "center", fontSize: 12, fontWeight: 600, color: "black"}}>Processing...</div>
+                <Footer />
+            </div>
+          : 
+          <div className={styles.grad}>
+            <div class="row">
+
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-4" >
+                <div style={style.content}>
+                <a href="/" className={styles.verifyTitle}>QuickMoney</a>
+                  <p></p>
+                  <hr></hr>
+                  <div style={{padding: 16, fontSize: 12, fontWeight: 600, color: "black"}}>Logged in as: {this.state.account}</div>
+
+                  <div style={{height: 80, margin: 10, marginBottom: 16, backgroundImage: "url(" + walletBg + ")",
+                  backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain'}} className={styles.buttonBox}>
+                    <div style={{paddingLeft: 110, paddingTop: 28}}
+                    className={styles.boxText}>
+                    BAL: ETH {window.web3.utils.fromWei(this.state.balance)}
+                    </div>
+                  </div>
+
+
+                  <div style={{height: 80, margin: 10, marginBottom: 16, backgroundImage: "url(" + interestBg + ")",
+                  backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain'}} className={styles.buttonBox}>
+                    <div style={{paddingLeft: 110, paddingTop: 28}}
+                    className={styles.boxText}>
+                    EMI: ETH {this.state.emi}
+                    </div>
+                  </div>
+
+                  <div style={{height: 10, margin: 18}}></div>
+
+                  <div className={styles.verifyMid} style={{marginBottom: 8}}>Quick Actions</div>
+
+                  <div class="row" style={{marginLeft: 42}}>
+                    <a href="/borrow" class="btn btn-light col-3" style={{height: 90, width: 40, backgroundImage: "url(" + borrow + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6, marginBottom: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                    <a href="/lend" class="btn btn-light col-3" style={{height: 90, width: 40, backgroundImage: "url(" + lend + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                    <a href="/requests" class="btn btn-light col-3" style={{height: 90, width: 40, backgroundImage: "url(" + requests + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6, marginBottom: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                  </div>
+
+                  <div class="row" style={{marginLeft: 42}}>
+                    <a href="/proposals" class="btn btn-outline-success col-3" style={{height: 90, width: 40, backgroundImage: "url(" + proposals + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                    <a href="/loans" class="btn btn-light col-3" style={{height: 90, width: 40, backgroundImage: "url(" + split + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                    <a href="/about" class="btn btn-light col-3" style={{height: 90, width: 40, backgroundImage: "url(" + help + ")", backgroundPosition: 'center', backgroundSize: "contain",
+                    backgroundRepeat: 'no-repeat', resizeMode: 'contain', margin: 6,
+                    borderRadius: 10, boxShadow: "0px 0px 5px #D2D3D8"}}>
+                    </a>
+                  </div>
+
+                </div>
+                
+              </div>
+
+              <div class="col-lg-8 col-md-8 col-sm-8 col-8">
+              <Navigation account={this.state.account}/>
+                <div style={{marginTop: 40, height: 600, backgroundImage: "url(" + bg + ")", backgroundPosition: 'center', backgroundSize: "contain",
+              backgroundRepeat: 'no-repeat', resizeMode: 'contain'}}>
+                  <div className={styles.verifyUpperMid}
+                  style={{paddingTop:80, paddingLeft: 40}}>Proposals</div>
+
+                  <p className={styles.desc}
+                  style={{paddingTop:20, paddingLeft: 40, paddingRight: 30}}>
+                    Track proposals for your loan requests. After evaluating the proposed terms, you can accept or deny the proposal. 
+                    Acceptance would immediately transfer the loan amount to your wallet and start the payment cycle. You cannot opt out
+                    of a loan cycle once accepted till it is completed.
+                  </p>
+                    
+                  <div className="content" style={{marginTop:4, paddingRight: 40, paddingLeft: 40}}>
+
+                    { this.state.requests.map((request, key) => {
+                    return(
+                        
+                    <div className="card border-success mb-3" key={key} >
+
+                        <div className="card-header">
+                        <small className="text-muted">Proposal ID: {request.id.toString()}</small>
+                        <p></p>
+                        <small className="text-muted">ID of Loan: {(request.loanId.toString())}</small>
+                        <p></p>
+                        <small className="text-muted">Lender: {(request.loaner.toString())}</small>
+                        <p></p>
+                        <small className="text-muted">Value: {window.web3.utils.fromWei(request.value.toString(), 'Ether')} ETH</small>
+                        </div>
+                        <ul id="certificateList" className="list-group list-group-flush">
+                        <li key={key} className="list-group-item py-3">
+
+                        <p></p>
+                        <small className="text-muted">Term Period: {(request.months.toString())} Months</small>
+                        <p></p>
+                        <small className="text-muted">Interest Rate: {(request.interest.toString())} %</small>
+                            
+                            <button
+                            className="btn btn-outline-danger btn-sm float-right pt-0"
+                            style={{marginLeft: 14}}
+                            name={request.identity}
+                            onClick={(event) => {
+                                this.declineRequest(request.loanId.toString(), request.id.toString(), request.value.toString())
+                            }}
+                            >
+                            Decline Proposal
+                            </button>
+
+                            <button
+                            className="btn btn-outline-success btn-sm float-right pt-0"
+                            name={request.identity}
+                            onClick={(event) => {
+                              const emi = this.emiCalc(
+                                window.web3.utils.fromWei(request.value.toString(), 'Ether'),
+                                request.interest,
+                                request.months
+                              )
+                              const set = emi * request.months
+                              this.approveRequest(request.loanId.toString(), request.id.toString(), window.web3.utils.toWei(set.toString(), 'Ether'))
+                            }}
+                            >
+                            Approve Proposal
+                            </button>
+                        </li>
+                        </ul>
+                    </div>
+                    )
+                })}
+                </div>
+                <Footer />
+                </div>
+                
+              </div>
+              
+            </div>
+            
+          </div>
+        }
       </div>
     );
   }
